e324d04107b4bc084a949ac9dcb1709247f94c14
Ali-Maia <aliciaengcomp@gmail.com>
2026-06-26 17:54:49 -0300

    feat: mapeamento de categorias em js/config.js

---
+++ js/config.js
@@ -0,0 +1,29 @@
+window.VDC_CONFIG = {
+  categorias: {
+    eletronicos: {
+      nome: 'Eletrônicos',
+      icone: '💻',
+      categorias: [],
+    },
+    casa: {
+      nome: 'Casa & Decor',
+      icone: '🏠',
+      categorias: [],
+    },
+    moda: {
+      nome: 'Moda',
+      icone: '👗',
+      categorias: [],
+    },
+    calcados: {
+      nome: 'Calçados',
+      icone: '👟',
+      categorias: [],
+    },
+    pet: {
+      nome: 'Mundo Pet',
+      icone: '🐾',
+      categorias: [],
+    },
+  }
+};
